8f3b025cbc445e7acb4bfac68188917e9c3b09c0
Robin Chowdhury <robin.chowdhury@tink.se>
2017-04-14 20:19:55 +0200

    fea(Perculation) Added methods that the evaluator will call

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,40 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Percolation {
+
+    public Percolation() {
+    }
+
+    // open site (row, col) if it is not open already
+    void open(int row, int col) {
+        throw new NotImplementedException();
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        throw new NotImplementedException();
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        throw new NotImplementedException();
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        throw new NotImplementedException();
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        throw new NotImplementedException();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
