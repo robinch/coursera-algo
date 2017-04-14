4c870d80700f9b1fdf4d0dda1cf5e014101712da
Robin Chowdhury <robin.chowdhury@tink.se>
2017-04-14 21:22:28 +0200

    fea(PercolationStats) Created PercolationStats

---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,33 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class PercolationStats {
+
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        throw new NotImplementedException();
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        throw new NotImplementedException();
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        throw new NotImplementedException();
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        throw new NotImplementedException();
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+    }
+}
